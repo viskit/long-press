c056f1b55935d53c62385afbd2568914596ae349
liangzeng <lionzale@gmail.com>
2021-08-23 16:32:31 +0800

    root node once register

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,53 @@
-export const register = (dom: Document | Element | ShadowRoot = document) => {
-  let startX = 0,
-    startY = 0,
-    ct;
+const rootMap = new Map();
 
-  const pointerdownHandle = (e: PointerEvent) => {
-    startX = e.clientX;
-    startY = e.clientY;
+export const register = (
+  dom: Document | Element | ShadowRoot = document
+): (() => void) | void => {
+  if (!rootMap.has(dom.getRootNode())) {
+    let startX = 0,
+      startY = 0,
+      ct;
 
-    const target = e.target as HTMLElement;
-    const delay = target.dataset.delay ? +target.dataset.delay : 300;
-    const path = e.composedPath();
-    ct = setTimeout(() => {
-        target.dispatchEvent(new PointerEvent("long-press", {clientX:startX,clientY:startY}));
-    }, delay);
-  };
+    const pointerdownHandle = (e: PointerEvent) => {
+      startX = e.clientX;
+      startY = e.clientY;
 
-  const pointermoveHandle = (e: PointerEvent) => {
-    var ox = Math.abs(startX - e.clientX);
-    var oy = Math.abs(startY - e.clientY);
+      const target = e.target as HTMLElement;
+      const delay = target.dataset.delay ? +target.dataset.delay : 300;
+      const path = e.composedPath();
+      ct = setTimeout(() => {
+        target.dispatchEvent(
+          new PointerEvent("long-press", { clientX: startX, clientY: startY })
+        );
+      }, delay);
+    };
 
-    if (ox >= 10 || oy >= 10) {
+    const pointermoveHandle = (e: PointerEvent) => {
+      var ox = Math.abs(startX - e.clientX);
+      var oy = Math.abs(startY - e.clientY);
+
+      if (ox >= 10 || oy >= 10) {
+        clearTimeout(ct);
+      }
+    };
+
+    const clear = () => {
       clearTimeout(ct);
-    }
-  };
+    };
 
-  const clear = () => {
-    clearTimeout(ct);
-  };
- 
-  dom.addEventListener("pointerdown", pointerdownHandle, false);
+    dom.addEventListener("pointerdown", pointerdownHandle, false);
 
-  dom.addEventListener("pointermove", pointermoveHandle, false);
+    dom.addEventListener("pointermove", pointermoveHandle, false);
 
-  dom.addEventListener("pointerup", clear, false);
+    dom.addEventListener("pointerup", clear, false);
 
-  dom.addEventListener("pointercancel", clear, false);
+    dom.addEventListener("pointercancel", clear, false);
 
-  return () => {
-    dom.removeEventListener("pointerdown", pointerdownHandle, false);
-    dom.removeEventListener("pointermove", pointermoveHandle, false);
-    dom.removeEventListener("pointerup", clear, false);
-    dom.removeEventListener("pointercancel", clear, false);
-  };
+    return () => {
+      dom.removeEventListener("pointerdown", pointerdownHandle, false);
+      dom.removeEventListener("pointermove", pointermoveHandle, false);
+      dom.removeEventListener("pointerup", clear, false);
+      dom.removeEventListener("pointercancel", clear, false);
+    };
+  }
 };
